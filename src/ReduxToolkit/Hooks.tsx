5d5a04cdfc3b0633855e63b2ea6b06f31ea960fc
Krish Gediya <gediyakrish144@gmail.com>
2025-02-22 09:37:36 +0530

    push

--- src/ReduxToolkit/Hooks.tsx
+++
@@ -1,6 +0,0 @@
-import type { TypedUseSelectorHook } from "react-redux";
-import { useDispatch, useSelector } from "react-redux";
-import type { AddDispatch, RootState } from "./Store";
-
-export const useAppDispatch: () => AddDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/Routers/PrivateRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAppSelector } from "../ReduxToolkit/Hooks";
-
-const PrivateRoute = () => {
-  const { isAuthenticated } = useAppSelector((start) => start.auth);
-  return isAuthenticated ? <Navigate to="/home" replace /> : <Outlet />;
-};
-
-export default PrivateRoute;
